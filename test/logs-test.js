28a77f41a032628da073e88777fabc24764f34c3
Joscha Feth <joscha@feth.com>
2014-04-30 16:05:05 +0200

    refactor logging tests to actually use asserts

--- test/logs-test.js
+++ test/logs-test.js
@@ -1,25 +1,71 @@
 /* global describe, it, beforeEach, afterEach */
 'use strict';
 
-var watch = require('..'),
-    path = require('path'),
+var proxyquire = require('proxyquire'),
+    gutil = require('gulp-util'),
+    gutilStub = {
+        log: function() {}
+    },
+    watch = proxyquire('..', {
+        'gulp-util': gutilStub
+    }),
+    sinon = require('sinon'),
     should = require('should'),
     gulp = require('gulp');
 
 describe('logging', function () {
-    beforeEach(function() { console.log(); })
+    beforeEach(function() {
+        sinon.spy(gutilStub, 'log');
+    });
+
+    afterEach(function() {
+        gutilStub.log.restore();
+    });
+
+    function assertSimpleCall() {
+        gutilStub.log.calledTwice.should.be.true;
+
+        var firstCall = gutilStub.log.firstCall;
+        var secondCall = gutilStub.log.secondCall;
+
+        firstCall.args[0].should.equal(gutil.colors.magenta('test.js'));
+        firstCall.args[1].should.equal('was');
+        firstCall.args[2].should.equal('added to watch');
+
+        secondCall.args[0].should.equal(gutil.colors.magenta('1 file'));
+        secondCall.args[1].should.equal('was');
+        secondCall.args[2].should.equal('added from pipe');
+    }
 
     it('should work when verbose is true', function (done) {
-        var w = gulp.src('test/fixtures/test.js').pipe(watch({ verbose: true, silent: false }))
+        var path = 'test/fixtures/test.js';
+        var w = gulp.src(path).pipe(watch({ verbose: true, silent: false }));
         w.on('finish', function () {
+            assertSimpleCall();
             w.on('end', done);
             w.close();
         });
     });
 
     it('should work when name is specified', function (done) {
-        var w = gulp.src('test/fixtures/test.js').pipe(watch({ name: 'Test', verbose: true, silent: false }))
+        var name = 'Test';
+        var w = gulp.src('test/fixtures/test.js').pipe(watch({ name: name, verbose: true, silent: false }));
         w.on('finish', function () {
+            gutilStub.log.calledTwice.should.be.true;
+
+            var firstCall = gutilStub.log.firstCall;
+            var secondCall = gutilStub.log.secondCall;
+
+            firstCall.args[0].should.equal(gutil.colors.cyan(name) + ' saw');
+            firstCall.args[1].should.equal(gutil.colors.magenta('test.js'));
+            firstCall.args[2].should.equal('was');
+            firstCall.args[3].should.equal('added to watch');
+
+            secondCall.args[0].should.equal(gutil.colors.cyan(name) + ' saw');
+            secondCall.args[1].should.equal(gutil.colors.magenta('1 file'));
+            secondCall.args[2].should.equal('was');
+            secondCall.args[3].should.equal('added from pipe');
+
             w.on('end', done);
             w.close();
         });
